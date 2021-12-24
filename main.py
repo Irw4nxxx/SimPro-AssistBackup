811e92e09ecc13bf96c7b54345515c6b5eace484
Irw4nxxx <95832801+Irw4nxxx@users.noreply.github.com>
2021-12-24 22:47:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from linepy import *
-from akad import *
+from lib.linepy import *
+from lib.akad import *
 from akad.ttypes import OpType, Message, TalkException
 from thrift.Thrift import *
 from thrift.unverting import *
@@ -15,12 +15,12 @@ from data import commands
 OT = OpType
 
 fileName = os.path.splitext(os.path.basename(__file__))[0]
-db = livejson.File("token/%s.json" %fileName)
+db = livejson.File("token1/%s.json" %fileName)
 
 if ":" in db['token']:
     app = "ANDROIDLITE\t2.11.1\tAndroid OS\t5.1.1"
 else:
-    app = "DESKTOPWIN\t6.0.3\tWindows\t10"
+    app = "ANDROIDLITE\t2.11.1\tAndroid OS\t5.1.1"
 try:
     client = LINE(idOrAuthToken=db["token"], appName=app)
 except:
@@ -31,7 +31,7 @@ except:
 uid = client.profile.mid
 poll = OEPoll(client)
 good = commands(fileName, client, app, uid)
-print("LOGIN SUCCESS")
+print("UID: {}\n\nApp: {}".format(uid,app))
 def main_loop(op):
     if op.type == OT.RECEIVE_MESSAGE: good.receive_message(op)
     elif op.type == OT.INVITE_INTO_GROUP or op.type == 121: good.notif_invite(op)
